328a7fdac79c57bf91ce2e223874a57b91540063
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-08-03 16:54:19 -0300

    Comparator problem

---
+++ src/hack/Comparator_Problem.java
@@ -0,0 +1,54 @@
+package hack;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+/*
+ * https://www.hackerrank.com/challenges/java-comparator/problem
+ */
+
+class Checker implements Comparator<Player> {
+
+	@Override
+	public int compare(Player p1, Player p2) {
+		if (p1.score == p2.score) {
+			return p1.name.compareTo(p2.name);
+		}
+		return p2.score - p1.score;
+	}
+
+}
+
+class Player {
+
+	String name;
+	int score;
+
+	Player(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+}
+
+public class Comparator_Problem {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+
+		Player[] player = new Player[n];
+		Checker checker = new Checker();
+
+		for (int i = 0; i < n; i++) {
+			player[i] = new Player(scan.next(), scan.nextInt());
+		}
+		scan.close();
+
+		Arrays.sort(player, checker);
+		for (int i = 0; i < player.length; i++) {
+			System.out.printf("%s %s\n", player[i].name, player[i].score);
+		}
+	}
+
+}
