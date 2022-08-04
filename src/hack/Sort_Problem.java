95604f1d6a9dd611b8e2e5f764ba89d26ccd2043
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-08-03 23:03:57 -0300

    Sort problem

---
+++ src/hack/Sort_Problem.java
@@ -0,0 +1,70 @@
+package hack;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+ * https://www.hackerrank.com/challenges/java-sort/problem
+ */
+
+class Student {
+	
+	private int id;
+	private String fname;
+	private double cgpa;
+
+	public Student(int id, String fname, double cgpa) {
+		super();
+		this.id = id;
+		this.fname = fname;
+		this.cgpa = cgpa;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public double getCgpa() {
+		return cgpa;
+	}
+	
+}
+
+public class Sort_Problem {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner in = new Scanner(System.in);
+		
+		int testCases = Integer.parseInt(in.nextLine());
+
+		List<Student> studentList = new ArrayList<Student>();
+		while (testCases > 0) {
+			int id = in.nextInt();
+			String fname = in.next();
+			double cgpa = in.nextDouble();
+
+			Student st = new Student(id, fname, cgpa);
+			studentList.add(st);
+
+			testCases--;
+		}
+
+		Collections.sort(studentList, Comparator
+				.comparing(Student::getCgpa)
+				.reversed().thenComparing(Student::getFname)
+				.thenComparing(Student::getId));;
+		
+		for (Student st : studentList) {
+			System.out.println(st.getFname());
+		}
+	}
+}
