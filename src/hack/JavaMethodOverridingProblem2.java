e519400043e3592f294d9f5558fffaaf82bdb72a
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-08-09 14:47:23 -0300

    Method overriding 2 problem

---
+++ src/hack/JavaMethodOverridingProblem2.java
@@ -0,0 +1,34 @@
+package hack;
+
+/*
+ * https://www.hackerrank.com/challenges/java-method-overriding-2-super-keyword/problem
+ */
+
+class BiCycle {
+	String define_me() {
+		return "a vehicle with pedals.";
+	}
+}
+
+class MotorCycle extends BiCycle {
+	String define_me() {
+		return "a cycle with an engine.";
+	}
+
+	MotorCycle() {
+		System.out.println("Hello I am a motorcycle, I am " + define_me());
+
+		// Fix this line
+		String temp = super.define_me(); 
+
+		System.out.println("My ancestor is a cycle who is " + temp);
+	}
+
+}
+
+public class JavaMethodOverridingProblem2 {
+
+	public static void main(String[] args) {
+		MotorCycle M = new MotorCycle();
+	}
+}
