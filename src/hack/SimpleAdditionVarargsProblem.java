1dde44d30db035a60dc1c3fdc76c07dd73013d56
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-08-11 15:12:07 -0300

    Simple addition varargs problem

---
+++ src/hack/SimpleAdditionVarargsProblem.java
@@ -0,0 +1,64 @@
+package hack;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * https://www.hackerrank.com/challenges/simple-addition-varargs/problem
+ */
+
+class Add {
+
+	public void add(int... arr) {
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (i == arr.length - 1) {
+				System.out.print(arr[i] + "=");
+			} else {
+				System.out.print(arr[i] + "+");
+			}
+			sum = sum + arr[i];
+		}
+		System.out.print(sum);
+		System.out.println();
+	}
+}
+
+public class SimpleAdditionVarargsProblem {
+
+	public static void main(String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			int n1 = Integer.parseInt(br.readLine());
+			int n2 = Integer.parseInt(br.readLine());
+			int n3 = Integer.parseInt(br.readLine());
+			int n4 = Integer.parseInt(br.readLine());
+			int n5 = Integer.parseInt(br.readLine());
+			int n6 = Integer.parseInt(br.readLine());
+			Add ob = new Add();
+			ob.add(n1, n2);
+			ob.add(n1, n2, n3);
+			ob.add(n1, n2, n3, n4, n5);
+			ob.add(n1, n2, n3, n4, n5, n6);
+			Method[] methods = Add.class.getDeclaredMethods();
+			Set<String> set = new HashSet<>();
+			boolean overload = false;
+			for (int i = 0; i < methods.length; i++) {
+				if (set.contains(methods[i].getName())) {
+					overload = true;
+					break;
+				}
+				set.add(methods[i].getName());
+
+			}
+			if (overload) {
+				throw new Exception("Overloading not allowed");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
