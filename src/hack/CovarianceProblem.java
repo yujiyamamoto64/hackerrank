09b2b6a0e40b2ab2eec9ede061ad916b2ecf5d52
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-08-17 10:55:18 -0300

    Covariance problem

---
+++ src/hack/CovarianceProblem.java
@@ -0,0 +1,68 @@
+package hack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * https://www.hackerrank.com/challenges/java-covariance/problem
+ */
+
+class Flower {
+	public String whatsYourName() {
+		return "I have many names and types";
+	}
+}
+
+class Jasmine extends Flower{
+	@Override
+	public String whatsYourName() {
+		return Jasmine.class.getName();
+	}
+}
+
+class Lily extends Flower{
+	@Override
+	public String whatsYourName() {
+		return Lily.class.getName();
+	}
+}
+
+class Region {
+	public Flower yourNationalFlower() {
+		return new Flower();
+	}
+}
+
+class WestBengal extends Region{
+	@Override
+	public Flower yourNationalFlower() {
+		return new Jasmine();
+	}
+}
+
+class AndhraPradesh extends Region{
+	@Override
+	public Flower yourNationalFlower() {
+		return new Lily();
+	}
+}
+
+public class CovarianceProblem {
+
+	public static void main(String[] args) throws IOException {
+	      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	      String s = reader.readLine().trim();
+	      Region region = null;
+	      switch (s) {
+	        case "WestBengal":
+	          region = new WestBengal();
+	          break;
+	        case "AndhraPradesh":
+	          region = new AndhraPradesh();
+	          break;
+	      }
+	      Flower flower = region.yourNationalFlower();
+	      System.out.println(flower.whatsYourName());
+	    }
+}
