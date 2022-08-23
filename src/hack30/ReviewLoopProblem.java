5dd8a4692977ed55417bb231bd5aa8ad8c722db1
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-08-22 22:43:27 -0300

    Review loop problem

---
+++ src/hack30/ReviewLoopProblem.java
@@ -0,0 +1,37 @@
+package hack30;
+
+import java.util.Scanner;
+
+public class ReviewLoopProblem {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.nextLine();
+		
+		for (int i = 0; i < N ; i++) {
+			String string = sc.nextLine();
+			char[] charArray = string.toCharArray();
+
+			for (int j = 0; j < charArray.length; j++) {
+				if (j % 2 == 0) {
+					System.out.print(charArray[j]);
+				}
+			}
+
+			System.out.print(" ");
+
+			for (int j = 0; j < charArray.length; j++) {
+				if (j % 2 != 0) {
+					System.out.print(charArray[j]);
+				}
+			}
+
+			System.out.println();
+		}
+
+		sc.close();
+	}
+
+}
