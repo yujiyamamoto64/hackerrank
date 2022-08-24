1f7c875d494ccd990b49f3a3b5deac43f12729fb
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-08-23 22:21:45 -0300

    hack30 Arrays problem

---
+++ src/hack30/Arrays30Problem.java
@@ -0,0 +1,30 @@
+package hack30;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Arrays30Problem {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
+        
+        Collections.reverse(arr);
+
+        for (Integer number : arr) {
+        	System.out.print(number + " ");
+        }
+        
+        bufferedReader.close();
+    }
+}
