e2bf7d72e995b9c995792962d3de4d838cabf051
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-07 22:50:04 -0300

    Day 29, bitwise and

---
+++ src/hack30/Day29BitwiseAnd.java
@@ -0,0 +1,67 @@
+package hack30;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
+
+/*
+ * https://www.hackerrank.com/challenges/30-bitwise-and/problem
+ */
+
+class D29Result {
+
+    /*
+     * Complete the 'bitwiseAnd' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER N
+     *  2. INTEGER K
+     */
+
+    public static int bitwiseAnd(int N, int K) {
+    // Write your code here
+    	int res = 0;
+    	for (int i = 0; i<=N; i++) {
+    		for (int j = i+1; j<=N; j++) {
+    			int ans = i&j;
+    			if (ans > res && ans<K) res = ans;
+    		}
+    	}
+    	return res;
+    }
+
+}
+
+public class Day29BitwiseAnd {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int count = Integer.parseInt(firstMultipleInput[0]);
+
+                int lim = Integer.parseInt(firstMultipleInput[1]);
+
+                int res = D29Result.bitwiseAnd(count, lim);
+
+                bufferedWriter.write(String.valueOf(res));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
