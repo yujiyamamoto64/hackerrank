7361ec6f41aca28b2d9fd9e7b63943e349b56cb5
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-23 10:12:49 -0300

    Queue challange Palindrome

---
+++ src/hack/QueuesChallenge.java
@@ -0,0 +1,36 @@
+package hack;
+
+import java.util.LinkedList;
+
+public class QueuesChallenge {
+
+	public static void main(String[] args) {
+		System.out.println(checkForPalindrome("abccba"));
+		System.out.println(checkForPalindrome("Was it a car or a cat I saw?"));
+		System.out.println(checkForPalindrome("I did, did I?"));
+		System.out.println(checkForPalindrome("hello"));
+		System.out.println(checkForPalindrome("Don't nod"));
+	}
+	
+	public static boolean checkForPalindrome(String string) {
+		LinkedList<Character> stack = new LinkedList<Character>();
+		LinkedList<Character> queue = new LinkedList<Character>();
+		String lowerCase = string.toLowerCase();
+		
+		for (int i = 0; i < lowerCase.length(); i++) {
+			char c = lowerCase.charAt(i);
+			if (c >= 'a' && c <= 'z') {
+				queue.addLast(c);
+				stack.push(c);
+			}
+		}
+		
+		while (!stack.isEmpty()) {
+			if (!stack.pop().equals(queue.removeFirst())) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+}
