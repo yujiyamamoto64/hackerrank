0ac2f2ad0177d595eeaed2c93eb5806b33b5b039
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-26 16:39:19 -0300

    Hashmap jdk

--- src/hack/SimpleHashtable.java
+++ src/hack/SimpleHashtable.java
@@ -1,7 +1,10 @@
 package hack;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Map;
 
 class ChainedHashtable {
 	
@@ -194,7 +197,8 @@ public class SimpleHashtable {
 		Employee marySmith = new Employee("Mary", "Smith", 22);
 		Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
 
-		//SimpleHashtable ht = new SimpleHashtable();
+		/*
+		SimpleHashtable ht = new SimpleHashtable();
 		ChainedHashtable ht = new ChainedHashtable();
 		ht.put("Jones", janeJones);
 		ht.put("Doe", johnDoe);
@@ -203,15 +207,35 @@ public class SimpleHashtable {
 
 		ht.printHashtable();
 		
-		/*
 		System.out.println("Retrieve key Smith: " + ht.get("Smith"));
 
-		
-		ht.remove("Doe");
 		ht.remove("Jones");
 		ht.printHashtable();
 		
 		System.out.println("Retrieve key Smith: " + ht.get("Smith"));
 		*/
+		
+		Map<String, Employee> hashMap = new HashMap<String, Employee>();
+		hashMap.put("Jones", janeJones);
+		hashMap.put("Doe", johnDoe);
+		hashMap.put("Smith", marySmith);
+		//Employee employee = hashMap.put("Doe", mikeWilson);
+		Employee employee = hashMap.putIfAbsent("Doe", mikeWilson);
+		System.out.println(employee);
+		
+		System.out.println(hashMap.getOrDefault("someone", mikeWilson));
+		System.out.println(hashMap.remove("Jones"));
+		//System.out.println(hashMap.containsKey("Doe"));
+		//System.out.println(hashMap.containsValue(janeJones));
+		
+		/*
+		Iterator<Employee> iterator = hashMap.values().iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+		*/
+		
+		hashMap.forEach((k, v) -> System.out.println("Key = " + k + "Employee = " + v));
+		
 	}
 }
