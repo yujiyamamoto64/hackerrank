c8a4ef33da9c8dc2aba45e4bcc6576243b24ac7d
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-26 15:59:33 -0300

    hashtable chaining

--- src/hack/SimpleHashtable.java
+++ src/hack/SimpleHashtable.java
@@ -1,5 +1,79 @@
 package hack;
 
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+class ChainedHashtable {
+	
+	private LinkedList<StoredEmployee>[] hashtable;
+	
+	public ChainedHashtable() {
+		hashtable = new LinkedList[10];
+		for (int i = 0; i < hashtable.length; i++) {
+			hashtable[i] = new LinkedList<StoredEmployee>();
+		}
+	}
+	
+	public void put (String key, Employee employee) {
+		int hashedKey = hashKey(key);
+		hashtable[hashedKey].add(new StoredEmployee(key, employee));
+	}
+	
+	public Employee get(String key) {
+		int hashedKey = hashKey(key);
+		ListIterator<StoredEmployee> iterator = hashtable[hashedKey].listIterator();
+		StoredEmployee employee = null;
+		while (iterator.hasNext()) {
+			employee = iterator.next();
+			if (employee.key.equals(key)) {
+				return employee.employee;
+			}
+		}
+		return null;
+	}
+	
+	public Employee remove (String key) {
+		int hashedKey = hashKey(key);
+		ListIterator<StoredEmployee> iterator = hashtable[hashedKey].listIterator();
+		StoredEmployee employee = null;
+		int index = -1;
+		while (iterator.hasNext()) {
+			employee = iterator.next();
+			index++;
+			if (employee.key.equals(key)) {
+				break;
+			}
+		}
+		if (employee == null) {
+			return null;
+		} else {
+			hashtable[hashedKey].remove(index);
+			return employee.employee;
+		}
+	}
+
+	private int hashKey(String key) {
+		//return key.length() % hashtable.length;
+		return Math.abs(key.hashCode() % hashtable.length);
+	}
+	
+	public void printHashtable() {
+		for (int i = 0; i < hashtable.length; i++) {
+			if (hashtable[i].isEmpty()) {
+				System.out.println("Position " + i + ": empty");
+			} else {
+				System.out.print("Position " + i + ": ");
+				ListIterator<StoredEmployee> iterator = hashtable[i].listIterator();
+				while (iterator.hasNext()) {
+					System.out.print(iterator.next().employee);
+					System.out.print("->");
+				}
+				System.out.println("null");
+			}
+		}
+	}
+}
+
 class StoredEmployee {
 
 	public String key;
@@ -119,24 +193,25 @@ public class SimpleHashtable {
 		Employee johnDoe = new Employee("John", "Doe", 4567);
 		Employee marySmith = new Employee("Mary", "Smith", 22);
 		Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
-		Employee billEnd = new Employee("Bill", "End", 78);
 
-		SimpleHashtable ht = new SimpleHashtable();
+		//SimpleHashtable ht = new SimpleHashtable();
+		ChainedHashtable ht = new ChainedHashtable();
 		ht.put("Jones", janeJones);
 		ht.put("Doe", johnDoe);
 		ht.put("Wilson", mikeWilson);
 		ht.put("Smith", marySmith);
 
 		ht.printHashtable();
-
-		System.out.println("Retrieve key Wilson: " + ht.get("Wilson"));
+		
+		/*
 		System.out.println("Retrieve key Smith: " + ht.get("Smith"));
 
-		ht.remove("Wilson");
+		
+		ht.remove("Doe");
 		ht.remove("Jones");
 		ht.printHashtable();
 		
 		System.out.println("Retrieve key Smith: " + ht.get("Smith"));
-
+		*/
 	}
 }
