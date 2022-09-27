33d0f5374c66877d02996d07c2fe5a451b469b9d
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-27 13:34:14 -0300

    Recursive form, but expensive, binary search

--- src/hack/BinarySearch.java
+++ src/hack/BinarySearch.java
@@ -5,10 +5,17 @@ public class BinarySearch {
 	public static void main(String[] args) {
 		int[] intArray = { -22, -15, 1, 7, 20, 35, 55 };
 		
+		/*
 		System.out.println(iterativeBinarySearch(intArray, -15));
 		System.out.println(iterativeBinarySearch(intArray, 35));
 		System.out.println(iterativeBinarySearch(intArray, 8888));
 		System.out.println(iterativeBinarySearch(intArray, 1));
+		*/
+		
+		System.out.println(recursiveBinarySearch(intArray, -15));
+		System.out.println(recursiveBinarySearch(intArray, 35));
+		System.out.println(recursiveBinarySearch(intArray, 8888));
+		System.out.println(recursiveBinarySearch(intArray, 1));
 
 	}
 	
@@ -31,5 +38,28 @@ public class BinarySearch {
 		}
 		
 		return -1;
+	}
+	
+	public static int recursiveBinarySearch(int[] input, int value) {
+		return recursiveBinarySearch(input, 0, input.length, value);
+	}
+
+	private static int recursiveBinarySearch(int[] input, int start, int end, int value) {
+		if (start >= end) {
+			return -1;
+		}
+		
+		int midpoint = (start + end) / 2;
+		System.out.println("midpoint = " + midpoint);
+		
+		if (input[midpoint] == value) {
+			return midpoint;
+		}
+		else if (input[midpoint] < value) {
+			return recursiveBinarySearch(input, midpoint + 1, end, value);
+		}
+		else {
+			return recursiveBinarySearch(input, start, midpoint, value);
+		}
 	}
 }
